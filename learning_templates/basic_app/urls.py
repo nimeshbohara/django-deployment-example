c33af23afa72b925dd7782a3db1917f2f921607c
nimeshbohara <36210305+nimeshbohara@users.noreply.github.com>
2019-05-28 22:47:21 -0400

    'first'

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from basic_app import views
+
+
+# template tagging
+
+app_name = 'basic_app'
+
+urlpatterns=[
+    url('relative/',views.relative,name='relative'),
+    url('other/',views.other,name='other'),
+
+]
